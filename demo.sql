87de526cfcb58ed565733f300ff73f17f94d1629
aakruti06 <aakrutikmh@gmail.com>
2026-04-13 13:03:47 +0530

    initial commit

---
+++ demo.sql
@@ -0,0 +1,14 @@
+-- create table
+CREATE TABLE st (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30),
+	marks INT
+);
+
+-- insert data
+INSERT INTO st (name, marks) VALUES
+('FIG' 80),
+('PEAR', 90);
+
+SELECT * FROM st;
+
