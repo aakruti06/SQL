52890efde6fceadc66ae490a98389a2e50f5b80c
aakruti06 <aakrutikmh@gmail.com>
2026-04-14 06:33:12 +0530

    updated mini project with joins and relations

--- mini1.sql
+++ mini1.sql
@@ -3,18 +3,37 @@ CREATE TABLE emp(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(20),
 	m_salary INT,
-	department VARCHAR(20)
+	dept_id INT,
+	FOREIGN KEY (dept_id) REFERENCES department(dept_id)
 );
 
+INSERT INTO department (dept_name) VALUES
+('IT'),
+('HR'),
+('FINANCE'),
+('MGMT');
+
+--create another table
+CREATE TABLE department(
+        dept_id INT AUTO_INCREMENT PRIMARY KEY,
+        dept_name VARCHAR(20)
+);
+
+
 -- insert values
-INSERT INTO emp VALUES
-('aruna',80000,'IT'),
-('asmita',88000,'IT'),
-('jiya',85000,'HR'),
-('smriti',90000,'SALES'),
-('preeti',87000,'SALES'),
-('roshni',89000,'HR'),
-('siya',100000,'MGMT');
+INSERT INTO emp (name, m_salary, dept_id)  VALUES
+('aruna',80000,1),
+('asmita',88000,1),
+('jiya',85000,2),
+('smriti',90000,3),
+('preeti',87000,3),
+('roshni',89000,2),
+('siya',100000,4);
+
+SELECT e.name, e.m_salary, d.dept_name
+FROM emp e
+JOIN department d
+ON e.dept_id = d.dept_id;
 
 -- print
 SELECT * FROM emp;
@@ -22,13 +41,12 @@ SELECT * FROM emp;
 --use group by 
 SELECT COUNT(*)
 FROM emp
-GROUP BY salary;
+GROUP BY m_salary;
 
 SELECT COUNT(*)
 FROM emp
-GROUP BY salary
+GROUP BY m_salary
 HAVING COUNT(*) > 1;
 
-SELECT AVG(salary) FROM emp ORDER BY salary ASC;
+SELECT AVG(m_salary) FROM emp ORDER BY m_salary ASC;
 
- 
