4fd2298dc83d786ab7ddb52d457aa834c970ad4c
aakruti06 <aakrutikmh@gmail.com>
2026-05-02 01:01:46 +0530

    phone-contacts project

---
+++ practice.py
@@ -0,0 +1,100 @@
+# practice program - phone and contacts
+
+import mysql.connector
+
+#db connection
+conn = mysql.connector.connect(
+	host = "localhost",
+	user = "aakruti",
+	password = "akM@4225",
+	database = "python_project"
+)
+
+cursor = conn.cursor()
+
+#add phone
+def add_phone():
+	num = int(input("Enter phone code: "))
+	cursor.execute("INSERT INTO phone(self_num) VALUES (%s)", (num,))
+	conn.commit()
+	print("phone added")
+
+#add contact
+def add_contacts():
+	name = input("Enter name: ")
+	num = int(input("Enter number: "))
+	self_num = int(input("Enter phone code: "))
+		
+	query = "INSERT INTO contacts(name, num, self_num) VALUES (%s, %s, %s)"
+	cursor.execute(query, (name, num, self_num))
+	conn.commit()
+	print("contact added")
+
+#view contacts
+def view_contacts():
+	query = """
+	SELECT c.name, c.num, p.self_num
+	FROM phone p
+	JOIN contacts c
+	ON p.self_num = c.self_num
+	"""
+	cursor.execute(query)
+	for row in cursor.fetchall():
+		print(row)
+
+#count contacts by phone id
+def count_contacts():
+	pid = int(input("Enter phone id: "))
+
+	query = """
+	SELECT COUNT(*)
+	FROM phone p
+	JOIN contacts c
+	ON p.self_num = c.self_num
+	WHERE p.id = %s
+	"""
+
+	cursor.execute(query, (pid,))
+	print("Total contacts: ",cursor.fetchone()[0])
+
+#filter contacts by phone code
+def filter_contacts():
+	code = int(input("Enter phone code: "))
+
+	query = """
+	SELECT name, num
+	FROM contacts
+	WHERE self_num = %s
+	"""
+	cursor.execute(query,(code,))
+	for row in cursor.fetchall():
+		print(row)
+
+
+while True:
+	print("\n1. Add phone: ")
+	print("2. Add contacts ")
+	print("3. View contacts ")
+	print("4. Count contacts by phone id ")
+	print("5. Fliter by phone code ")
+	print("6. Exit ")
+
+	choice = input("Enter choice: ")
+	if choice == '1':
+		add_phone()
+	elif choice == '2':
+		add_contacts()
+	elif choice == '3':
+		view_contacts()
+	elif choice == '4':
+		count_contacts()
+	elif choice == '5':
+		filter_contacts()
+	elif choice == '6':
+		print("Exit...")
+		break
+	else:
+		print("Invalid choice")
+
+
+
